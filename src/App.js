2d548b91aa7c7b5a668fc45264b60870da9696d0
matheusmarks <matheus-marques@LAPTOP-K062DHUE.localdomain>
2021-07-21 19:39:35 -0300

    chore: Updating App.js to look like App.final.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { HeadCounter } from "./Components/Counter";
 import { FabButton } from "./Components/FabButton";
 import Navbar from "./Components/Navbar";
+import { RepositoryList } from "./Components/RepositortList";
 import { likesCounter } from "./Services/expensiveCalculation";
+
+const SEARCH = "https://api.github.com/search/repositories";
 
 function App() {
   const [totalLikes, setTotalLikes] = useState(0);
   const [dark, setDark] = useState(false);
 
-  const likes = likesCounter(totalLikes);
+  const getRepositories = React.useCallback((query) => {
+    return fetch(`${SEARCH}?q=${query}`);
+  }, []);
 
-  const theme = {
+  const toogleDarkmode = () => setDark(!dark);
+
+  const likes = useMemo(() => likesCounter(totalLikes), [totalLikes]);
+
+  const theme = useMemo(() => ({
     color: dark ? "#fff" : "#333",
     navbar: dark ? "#1a202c" : "#e5e7eb",
     backgroundColor: dark ? "#333" : "#fff",
-  };
+  }), [dark]);
 
-  const toogleDarkmode = () => setDark(!dark);
+
+
+  useEffect(() => console.log('Theme has changed !'), [theme]);
 
   return (
     <div style={theme} className="App">
       <Navbar theme={theme.navbar} toogleDarkmode={toogleDarkmode} />
       <HeadCounter likes={likes} />
+      <RepositoryList getRepositories={getRepositories} />
       <FabButton totalLikes={totalLikes} setTotalLikes={setTotalLikes} />
     </div>
   );
